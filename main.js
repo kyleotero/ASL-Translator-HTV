7bd768dbca218347a884a861ca150416bcdefdfc
kyleotero <kkyleotero@gmail.com>
2022-10-15 14:48:14 -0400

    added capability to parse full strings

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-url = "http://127.0.0.1:5000/getUrl"
+var url = "http://127.0.0.1:5000/getUrl"
+var vids
 
 function load() {
-    $.post(
-        url +
-        "?data=" +
-        JSON.stringify({
-            word: "f",
-        }),
-        response
-    );
+    var input = document.getElementById("input")
+    var words = input.value.split(" ")
+    vids = new Array(words.length)
+
+    $.ajaxSetup({ async: false });
+    for (var x = 0; x < words.length; x++) {
+        $.post(
+            url +
+            "?data=" +
+            JSON.stringify({
+                word: words[x],
+            }),
+            response
+        );
+    }
 }
 
 function response(data) {
-    console.log(data)
+    vids.push(data)
+    console.log(vids)
 }
