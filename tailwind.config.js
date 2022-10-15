d7bd3b35934dc5eacad2b9d6c67830bb968a6619
kyleotero <kkyleotero@gmail.com>
2022-10-15 09:27:20 -0400

    reconfigured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#aab7c2",
+        main: "#191b1d",
+      },
+      boxShadow: {
+        bot: "0px 0.8em 1.2em #26292b",
+        button: "0px 0.4em 0.8em #26292b",
+      },
+      spacing: {
+        "92%": "92%",
+        "95%": "95%",
+        diff: "screen - 83%",
+      },
+      fontSize: {
+        standard: "1vw",
+        mobile: "2vw",
+        med: "1.5vw",
+      },
+      content: {
+        pre: "attr(pref)",
+      },
+      fontFamily: {
+        jetBrains: ["jetBrains"],
+      },
+      borderWidth: {
+        3: "3px",
+      },
+      flex: {
+        midflex: "0 0 50%",
+      },
+    },
   },
   plugins: [],
-}
+};
